702893d2f2a0a718c7c72469349afe35fb3378ef
hasnainroopawalla <hasnain.roopawalla@gmail.com>
2022-02-02 14:30:12 +0100

    Removed faulty graph test

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -17,15 +17,6 @@ def test_get_nodes() -> None:
     assert G.get_all_nodes() == ["A", "B", "C"]
 
 
-def test_get_all_edges() -> None:
-    assert G.get_all_edges() == [
-        ("A", "B", 5),
-        ("A", "C", 8),
-        ("B", "C", 56),
-        ("C", "A", 33),
-    ]
-
-
 def test_get_neighbors() -> None:
     assert G.get_neighbors("A") == ["B", "C"]
 
